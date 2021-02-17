d83077848710c2059af96231fae9c8a64d6e6ba3
simon gunning <sigunning@hotmail.com>
2021-02-17 23:11:19 +0000

    Add project files.

---
+++ Breadmakers/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Breadmakers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Your Events page.";
+
+            return View();
+        }
+
+        public ActionResult Brethren()
+        {
+            ViewBag.Message = "Your Members page.";
+
+            return View();
+        }
+
+    }
+}
